578e5af1929132bb85ab50c147c0411fe11e7365
maxfoxim <nicefuchs@gmail.come>
2025-03-31 23:23:10 +0200

    InitalCommit

---
+++ OPCUA_Frontend_Editor/OPCUA_Frontend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OpcuaFrontendConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "OPCUA_Frontend"
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OPCUA_Frontend
 Frontend for setting OPCUA Nodes for Robogate
+
+# Ausführen
+ .venv/bin/python OPCUA_Frontend_Editor/manage.py runserver  5005
